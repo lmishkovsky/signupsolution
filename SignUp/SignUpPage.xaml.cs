8f049d8072784a2c847842cb5d207f32fe918735
lmishkovsky <brightsoftbg@outlook.com>
2017-09-22 18:15:54 +0300

    Initial check-in of module SignUpSolution

---
+++ SignUp/SignUpPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SignUp
+{
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
