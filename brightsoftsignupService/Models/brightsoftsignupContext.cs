95ee06510c8d072c72f393fead72d3957fcc5e7b
lmishkovsky <brightsoftbg@outlook.com>
2017-10-09 18:25:56 +0300

    Removed the extra DB set.

--- brightsoftsignupService/Models/brightsoftsignupContext.cs
+++ brightsoftsignupService/Models/brightsoftsignupContext.cs
@@ -23,7 +23,6 @@ namespace brightsoftsignupService.Models
         } 
 
         public DbSet<TodoItem> TodoItems { get; set; }
-        // public DbSet<GroupItem> GroupItems { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
