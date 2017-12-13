172d9f4e430349b85650714929c1960f73d8a109
lmishkovsky <brightsoftbg@outlook.com>
2017-12-13 19:13:47 +0200

    Added the floating action button renderers after cock up.

---
+++ SignUp.PCL/CustomControls/FloatingActionButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SignUp.PCL.CustomControls
+{
+    public class FloatingActionButton : Button
+    {
+        public static BindableProperty ButtonColorProperty = BindableProperty.Create(nameof(ButtonColor), typeof(Color), typeof(FloatingActionButton), Color.Accent);
+
+        public Color ButtonColor
+        {
+            get
+            {
+                return (Color)GetValue(ButtonColorProperty);
+            }
+            set
+            {
+                SetValue(ButtonColorProperty, value);
+            }
+        }
+    }
+}
