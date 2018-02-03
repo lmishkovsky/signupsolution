f1192b5e89a0ba66a619517a2bbd1cbcd7988d36
lmishkovsky <brightsoftbg@outlook.com>
2018-02-03 11:31:01 +0200

    new message view model code behind.

--- SignUp/ViewModels/NewMessagePageViewModel.cs
+++ SignUp/ViewModels/NewMessagePageViewModel.cs
@@ -1,10 +1,91 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Plugin.Settings;
+using SignUp.Abstractions;
+using SignUp.Models;
+using Xamarin.Forms;
+
 namespace SignUp.ViewModels
 {
-    public class NewMessagePageViewModel
+    /// <summary>
+    /// New message page view model.
+    /// </summary>
+    public class NewMessagePageViewModel : BaseViewModel
     {
-        public NewMessagePageViewModel()
+        string facebookID = string.Empty;
+        string facebookName = string.Empty;
+        string facebookEmail = string.Empty;
+        string groupCode = string.Empty;
+        DateTime dtNextEventDate;
+
+        string message = "Type your message here.";
+        public string Message
         {
+            get { return message; }
+            set
+            {
+                message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SignUp.ViewModels.NewMessagePageViewModel"/> class.
+        /// </summary>
+        public NewMessagePageViewModel(DateTime dtNextEventDate)
+        {
+            this.facebookID = CrossSettings.Current.GetValueOrDefault(Constants.CrossSettingsKeys.FacebookID, string.Empty);
+            this.facebookName = CrossSettings.Current.GetValueOrDefault(Constants.CrossSettingsKeys.FacebookName, string.Empty);
+            this.facebookEmail = CrossSettings.Current.GetValueOrDefault(Constants.CrossSettingsKeys.FacebookEmail, string.Empty);
+            this.groupCode = CrossSettings.Current.GetValueOrDefault(Constants.CrossSettingsKeys.GroupCode, string.Empty);
+
+            this.dtNextEventDate = dtNextEventDate;
+        }
+
+        Command btnCommand;
+
+        public Command AddNewItemCommand => btnCommand ?? (btnCommand = new Command(async () => await ExecuteAddNewItemCommand()));
+
+        /// <summary>
+        /// Executes the add new item command.
+        /// </summary>
+        /// <returns>The add new item command.</returns>
+        async Task ExecuteAddNewItemCommand()
+        {
+            if (IsBusy) return;
+
+            IsBusy = true;
+
+            try
+            {
+                var table = App.CloudService.GetTable<ForumItem>();
+
+                // prepare the new entry 
+                ForumItem newPost = new ForumItem();
+                newPost.GroupCode = this.groupCode;
+                newPost.UserID = this.facebookID;
+                newPost.Name = this.facebookName;
+                newPost.Email = this.facebookEmail;
+                newPost.EventDate = this.dtNextEventDate;
+                newPost.Message = this.Message;
+
+                // add the new signup
+                await table.CreateItemAsync(newPost);
+
+                this.Message = string.Empty;
+
+                // go back to previous page
+                await Xamarin.Forms.Application.Current.MainPage.Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[GroupCheck] Error = {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
