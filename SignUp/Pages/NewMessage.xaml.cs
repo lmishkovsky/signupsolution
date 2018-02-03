f772c9b4d4496e053ed90ac88bd3c50b5fb6c4ee
lmishkovsky <brightsoftbg@outlook.com>
2018-02-03 11:29:00 +0200

    Saving new page code behind.

--- SignUp/Pages/NewMessage.xaml.cs
+++ SignUp/Pages/NewMessage.xaml.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SignUp.ViewModels;
 using Xamarin.Forms;
 
 namespace SignUp.Pages
 {
     public partial class NewMessage : ContentPage
     {
-        public NewMessage()
+        public NewMessage(DateTime dtNextEventDate)
         {
             InitializeComponent();
+
+            BindingContext = new NewMessagePageViewModel(dtNextEventDate);
         }
     }
 }
